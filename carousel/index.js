a1b7bada62e26e754c140d67e417fa1cd8690bec
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-02-03 16:07:27 +0900

    [Carouse] complete add Img and Tap Button
    Img와 tap Button 이 추가되는 것까지 구현했다.

--- carousel/index.js
+++ carousel/index.js
@@ -1,18 +1,26 @@
 const inputValue = document.getElementById('numberToChange')
 const addNumber = document.getElementById('add')
 const subNumber = document.getElementById('sub')
-let ImgBox = document.getElementById('imageContainer')
-const newImg = document.querySelector('.plus')
-const plusSection = document.getElementById('plusSection')
+const imageContainer = document.getElementById('imageContainer')
+const tapButton = document.querySelector('.tapButton')
 
-const a = '<div>asdf<div/>'
+addNumber.addEventListener('click', function (plusImgTapButton) {
+  let numberToAdd = inputValue.value
+  let currentAllImg = parseInt(imageContainer.lastElementChild.getAttribute('class'), 10)
 
-addNumber.onclick = () => {
-  let numberToAdd = parseInt(inputValue.value, 10)
   inputValue.value = ''
-  console.log(ImgBox)
+
   for (let i = 0; i < numberToAdd; i++) {
-    plusSection.innerHTML = a
-    console.log(plusSection)
+    imageContainer.insertAdjacentHTML(
+      'beforeend',
+      `<div class="` +
+        `${i + 1 + currentAllImg}` +
+        `"><img class="plus" src="./assets/basis.png" alt="사진을 불러올 수 없습니다." /></div>`,
+    )
+
+    tapButton.insertAdjacentHTML(
+      'beforeend',
+      `<input type="radio" name="page" value="` + `${i + 1 + currentAllImg}` + `" />`,
+    )
   }
-}
+})
