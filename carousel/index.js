67c27ac50daab4c1e7290723d19467a77d483631
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-02-04 02:47:42 +0900

    [Carouse] complete All function
    모든 기능 구현. 코드 다시 수정 필요

--- carousel/index.js
+++ carousel/index.js
@@ -2,20 +2,28 @@ const inputValue = document.getElementById('numberToChange')
 const addNumber = document.getElementById('add')
 const subNumber = document.getElementById('sub')
 const imageContainer = document.getElementById('imageContainer')
+
+const rightButton = document.querySelector('#rightButton')
+const leftButton = document.querySelector('#leftButton')
 const tapButton = document.querySelector('.tapButton')
+let tapButtonList = tapButton.querySelectorAll('input')
 
 addNumber.addEventListener('click', function (plusImgTapButton) {
   let numberToAdd = inputValue.value
-  let currentAllImg = parseInt(imageContainer.lastElementChild.getAttribute('class'), 10)
-
+  let currentAllImg = imageContainer.childElementCount
+  let currentFirstImg = imageContainer.firstElementChild.getAttribute('class')
   inputValue.value = ''
 
   for (let i = 0; i < numberToAdd; i++) {
-    imageContainer.insertAdjacentHTML(
-      'beforeend',
+    let lastImg = imageContainer.querySelectorAll('div')[currentAllImg - currentFirstImg + i]
+    /* 만약 현재 내가 보고있는 위치가 애매한 3번이나 이런곳일 때, 넣어지는 순서가 꼬이지 않도록 넣어줌
+     */ lastImg.insertAdjacentHTML(
+      'afterend',
       `<div class="` +
         `${i + 1 + currentAllImg}` +
-        `"><img class="plus" src="./assets/basis.png" alt="사진을 불러올 수 없습니다." /></div>`,
+        `"><img class="plus" src="./assets/basis.png" alt="사진을 불러올 수 없습니다." /><strong>` +
+        `${i + 1 + currentAllImg}` +
+        `</strong></div>`,
     )
 
     tapButton.insertAdjacentHTML(
@@ -23,4 +31,47 @@ addNumber.addEventListener('click', function (plusImgTapButton) {
       `<input type="radio" name="page" value="` + `${i + 1 + currentAllImg}` + `" />`,
     )
   }
+  let tapButtonList = tapButton.querySelectorAll('input')
 })
+
+rightButton.addEventListener('click', function (rightMove) {
+  let first = imageContainer.firstElementChild
+  imageContainer.append(first) /* 왜 돼? 제일 앞 요소를 반화하면서 지우거나/ 뭐지?*/
+  let tapButtonList = tapButton.querySelectorAll('input')
+  for (let i = 0; i < 100; i++) {
+    if (imageContainer.firstElementChild.getAttribute('class') === tapButtonList[i].value) {
+      tapButtonList[i].checked = true
+      break
+    }
+  }
+})
+
+leftButton.addEventListener('click', function (leftMove) {
+  let last = imageContainer.lastElementChild
+  imageContainer.prepend(last) /* 왜 돼? 제일 앞 요소를 반화하면서 지우거나/ 뭐지?*/
+  let tapButtonList = tapButton.querySelectorAll('input')
+  for (let i = 0; i < 100; i++) {
+    if (imageContainer.firstElementChild.getAttribute('class') === tapButtonList[i].value) {
+      tapButtonList[i].checked = true
+      break
+    }
+  }
+})
+
+tapButton.addEventListener('click', function (tapClick) {
+  let targetTap = tapClick.target.value
+  for (let i = 0; i < imageContainer.childElementCount; i++) {
+    if (imageContainer.firstElementChild.getAttribute('class') === targetTap) {
+      break
+    }
+    let first = imageContainer.firstElementChild
+    imageContainer.append(first)
+  }
+})
+
+for (let i = 0; i < 100; i++) {
+  if (imageContainer.firstElementChild.getAttribute('class') === tapButtonList[i].value) {
+    tapButtonList[i].checked = true
+    break
+  }
+}
