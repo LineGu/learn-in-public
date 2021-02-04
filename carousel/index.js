1d1e976228ad74dd50f9c18485229325720bdff7
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-02-04 16:42:55 +0900

    [Refactor] Refactor html class name and index.js
    html 의 class 이름과 index.js 파일을 좀 더 간결하게 리팩토링하였다.

--- carousel/index.js
+++ carousel/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const numberToAddElem = document.querySelector('#numberToAdd');
 const addButtonElem = document.querySelector('#add');
 const imageContainerElem = document.querySelector('#sliderContent');
@@ -102,3 +103,74 @@ initialCheckTabButton();
 addButtonElem.addEventListener('click', plusImgItem);
 arrowBoxElem.addEventListener('click', moveByArrow);
 tabButtonElem.addEventListener('click', moveByTab);
+=======
+const inputValue = document.querySelector('#numberToAdd')
+const addNumber = document.querySelector('#add')
+const imageContainer = document.querySelector('#sliderContent')
+const tapButton = document.querySelector('.tapButtonBox')
+const ArrowBox = document.querySelector('.ArrowAndImgBox')
+
+const makeImgToAdd = (number) => {
+  return (
+    `<div class="` +
+    `${number}` +
+    `"><img class="plus" src="./assets/basis.png" alt="사진을 불러올 수 없습니다." /><strong>` +
+    `${number}` +
+    `</strong></div>`
+  )
+}
+const makeTabToAdd = (number) => {
+  return `<input type="radio" name="page" value="` + `${number}` + `" />`
+}
+
+const tabbuttonCheck = (index) => {
+  const tapButtonList = tapButton.querySelectorAll('input')
+  tapButtonList[index].checked = true
+}
+
+const plusImgItem = () => {
+  const numberToAdd = inputValue.value
+  const currentAllImg = imageContainer.childElementCount
+  const currentFirstImg = imageContainer.firstElementChild.getAttribute('class')
+  inputValue.value = ''
+
+  for (let i = 0; i < numberToAdd; i++) {
+    const lastImg = imageContainer.querySelectorAll('div')[currentAllImg - currentFirstImg + i]
+    lastImg.insertAdjacentHTML('afterend', makeImgToAdd(i + currentAllImg + 1))
+    tapButton.insertAdjacentHTML('beforeend', makeTabToAdd(i + currentAllImg + 1))
+  }
+}
+
+const moveByArrow = (event) => {
+  const first = imageContainer.firstElementChild
+  const last = imageContainer.lastElementChild
+  const direction = event.target.id
+
+  switch (direction) {
+    case 'rightButton':
+      imageContainer.append(first)
+      break
+
+    case 'leftButton':
+      imageContainer.prepend(last)
+      break
+  }
+  tabbuttonCheck(imageContainer.firstElementChild.getAttribute('class') - 1)
+}
+
+const moveByTab = (event) => {
+  const targetTap = event.target.value
+  for (let i = 0; i < imageContainer.childElementCount; i++) {
+    if (imageContainer.firstElementChild.getAttribute('class') === targetTap) {
+      break
+    }
+    let first = imageContainer.firstElementChild
+    imageContainer.append(first)
+  }
+}
+
+tapButton.firstElementChild.checked = true
+addNumber.addEventListener('click', plusImgItem)
+ArrowBox.addEventListener('click', moveByArrow)
+tapButton.addEventListener('click', moveByTab)
+>>>>>>> d48e41b ([Repactory] Repactory html class name and index.js)
