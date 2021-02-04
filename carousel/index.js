d48e41b6dff00f6382645471b4ed580c50296ce7
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-02-04 16:42:55 +0900

    [Repactory] Repactory html class name and index.js
    html 의 class 이름과 index.js 파일을 좀 더 간결하게 리팩토링하였다.

--- carousel/index.js
+++ carousel/index.js
@@ -1,65 +1,59 @@
-const inputValue = document.getElementById('numberToChange')
-const addNumber = document.getElementById('add')
-const subNumber = document.getElementById('sub')
-const imageContainer = document.getElementById('imageContainer')
+const inputValue = document.querySelector('#numberToAdd')
+const addNumber = document.querySelector('#add')
+const imageContainer = document.querySelector('#sliderContent')
+const tapButton = document.querySelector('.tapButtonBox')
+const ArrowBox = document.querySelector('.ArrowAndImgBox')
 
-const rightButton = document.querySelector('#rightButton')
-const leftButton = document.querySelector('#leftButton')
-const tapButton = document.querySelector('.tapButton')
-let tapButtonList = tapButton.querySelectorAll('input')
+const makeImgToAdd = (number) => {
+  return (
+    `<div class="` +
+    `${number}` +
+    `"><img class="plus" src="./assets/basis.png" alt="사진을 불러올 수 없습니다." /><strong>` +
+    `${number}` +
+    `</strong></div>`
+  )
+}
+const makeTabToAdd = (number) => {
+  return `<input type="radio" name="page" value="` + `${number}` + `" />`
+}
 
-addNumber.addEventListener('click', function (plusImgTapButton) {
-  let numberToAdd = inputValue.value
-  let currentAllImg = imageContainer.childElementCount
-  let currentFirstImg = imageContainer.firstElementChild.getAttribute('class')
+const tabbuttonCheck = (index) => {
+  const tapButtonList = tapButton.querySelectorAll('input')
+  tapButtonList[index].checked = true
+}
+
+const plusImgItem = () => {
+  const numberToAdd = inputValue.value
+  const currentAllImg = imageContainer.childElementCount
+  const currentFirstImg = imageContainer.firstElementChild.getAttribute('class')
   inputValue.value = ''
 
   for (let i = 0; i < numberToAdd; i++) {
-    let lastImg = imageContainer.querySelectorAll('div')[currentAllImg - currentFirstImg + i]
-    /* 만약 현재 내가 보고있는 위치가 애매한 3번이나 이런곳일 때, 넣어지는 순서가 꼬이지 않도록 넣어줌
-     */ lastImg.insertAdjacentHTML(
-      'afterend',
-      `<div class="` +
-        `${i + 1 + currentAllImg}` +
-        `"><img class="plus" src="./assets/basis.png" alt="사진을 불러올 수 없습니다." /><strong>` +
-        `${i + 1 + currentAllImg}` +
-        `</strong></div>`,
-    )
-
-    tapButton.insertAdjacentHTML(
-      'beforeend',
-      `<input type="radio" name="page" value="` + `${i + 1 + currentAllImg}` + `" />`,
-    )
+    const lastImg = imageContainer.querySelectorAll('div')[currentAllImg - currentFirstImg + i]
+    lastImg.insertAdjacentHTML('afterend', makeImgToAdd(i + currentAllImg + 1))
+    tapButton.insertAdjacentHTML('beforeend', makeTabToAdd(i + currentAllImg + 1))
   }
-  let tapButtonList = tapButton.querySelectorAll('input')
-})
+}
 
-rightButton.addEventListener('click', function (rightMove) {
-  let first = imageContainer.firstElementChild
-  imageContainer.append(first) /* 왜 돼? 제일 앞 요소를 반화하면서 지우거나/ 뭐지?*/
-  let tapButtonList = tapButton.querySelectorAll('input')
-  for (let i = 0; i < 100; i++) {
-    if (imageContainer.firstElementChild.getAttribute('class') === tapButtonList[i].value) {
-      tapButtonList[i].checked = true
+const moveByArrow = (event) => {
+  const first = imageContainer.firstElementChild
+  const last = imageContainer.lastElementChild
+  const direction = event.target.id
+
+  switch (direction) {
+    case 'rightButton':
+      imageContainer.append(first)
       break
-    }
-  }
-})
 
-leftButton.addEventListener('click', function (leftMove) {
-  let last = imageContainer.lastElementChild
-  imageContainer.prepend(last) /* 왜 돼? 제일 앞 요소를 반화하면서 지우거나/ 뭐지?*/
-  let tapButtonList = tapButton.querySelectorAll('input')
-  for (let i = 0; i < 100; i++) {
-    if (imageContainer.firstElementChild.getAttribute('class') === tapButtonList[i].value) {
-      tapButtonList[i].checked = true
+    case 'leftButton':
+      imageContainer.prepend(last)
       break
-    }
   }
-})
+  tabbuttonCheck(imageContainer.firstElementChild.getAttribute('class') - 1)
+}
 
-tapButton.addEventListener('click', function (tapClick) {
-  let targetTap = tapClick.target.value
+const moveByTab = (event) => {
+  const targetTap = event.target.value
   for (let i = 0; i < imageContainer.childElementCount; i++) {
     if (imageContainer.firstElementChild.getAttribute('class') === targetTap) {
       break
@@ -67,11 +61,9 @@ tapButton.addEventListener('click', function (tapClick) {
     let first = imageContainer.firstElementChild
     imageContainer.append(first)
   }
-})
-
-for (let i = 0; i < 100; i++) {
-  if (imageContainer.firstElementChild.getAttribute('class') === tapButtonList[i].value) {
-    tapButtonList[i].checked = true
-    break
-  }
 }
+
+tapButton.firstElementChild.checked = true
+addNumber.addEventListener('click', plusImgItem)
+ArrowBox.addEventListener('click', moveByArrow)
+tapButton.addEventListener('click', moveByTab)
